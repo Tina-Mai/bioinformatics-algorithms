ce970f41b03330fd628f93c34e1621e89874418a
Tina Mai <tinabmai@gmail.com>
2025-01-02 17:31:48 -0800

    frequent_words

--- replication_ori.py
+++ replication_ori.py
@@ -1,4 +1,6 @@
-# FIND NUMBER OF REPEATING STRINGS IN A GENOMIC SEQUENCE
+# HIDDEN MESSAGE PROBLEM: find a “hidden message” (frequently recurring strings) in the replication origin
+# k-mer: a substring of length k
+# Count(Text, Pattern): number of times that a k-mer Pattern appears as a substring of Text
 def pattern_count(text: str, pattern: str) -> int:
     count = 0
     for i in range(0, len(text) - len(pattern) + 1):
@@ -6,3 +8,27 @@ def pattern_count(text: str, pattern: str) -> int:
             count += 1
     return count
 
+
+# FREQUENT WORDS PROBLEM: Find the most frequent k-mers in a string
+# most frequent k-mer: the Pattern that maximizes Count(Text, Pattern) in Text
+# FrequencyTable(Text, k): a map of k-mers to their counts in the text
+# FrequentWords(Text, k): a list of the most frequent k-mers in the text
+def frequency_table(text: str, k: int) -> dict[str, int]:
+    freq_map = {}
+    n = len(text)
+    for i in range(0, n - k + 1):
+        pattern = text[i:i + k]
+        if pattern not in freq_map:
+            freq_map[pattern] = 1
+        else:
+            freq_map[pattern] += 1
+    return freq_map
+
+def frequent_words(text: str, k: int) -> list[str]:
+    frequent_patterns = []
+    freq_map = frequency_table(text, k)
+    max_count = max(freq_map.values())
+    for pattern in freq_map:
+        if freq_map[pattern] == max_count:
+            frequent_patterns.append(pattern)
+    return frequent_patterns
